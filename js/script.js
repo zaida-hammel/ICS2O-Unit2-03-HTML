b381d8da7cb5803ede8952a85f0de0fdec487d20
zaida-hammel <99354763+zaida-hammel@users.noreply.github.com>
2022-03-22 16:54:04 +0000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Created by: Zaida Hammel
+// Created on: March 2022
+// This file contains the JS functions for index.html
+
+/**
+ * This function gets users street name and street number and shows it back to user.
+ */
+function enterClicked() {
+  // input
+  const streetName = document.getElementById("street-name").value
+  const streetNumber = parsInt(document.getElementbyId("street-number").value)
+
+  // output
+  document.getElementById("address").innerHTML =
+    "Your adress is: " + streetName + streetNumber + "."
+}
