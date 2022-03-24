7c7410951f062bfa671288ada6e355d4f18c2a99
zaida-hammel <99354763+zaida-hammel@users.noreply.github.com>
2022-03-24 16:37:16 +0000

    edited js script

--- js/script.js
+++ js/script.js
@@ -8,9 +8,9 @@
 function enterClicked() {
   // input
   const streetName = document.getElementById("street-name").value
-  const streetNumber = parsInt(document.getElementById("street-number").value)
+  const streetNumber = parseInt(document.getElementById("street-number").value)
 
   // output
   document.getElementById("address").innerHTML =
-    "Your address is: " + streetName + streetNumber + "."
+    "Your address is: " + streetName + " " + streetNumber + "."
 }
